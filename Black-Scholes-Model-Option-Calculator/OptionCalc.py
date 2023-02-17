0a9f7e944a7cdd6cad132eca24d4d1ad6229eb2c
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2023-02-17 10:03:55 -0500

    Update OptionCalc.py

--- Black-Scholes-Model-Option-Calculator/OptionCalc.py
+++ Black-Scholes-Model-Option-Calculator/OptionCalc.py
@@ -10,8 +10,8 @@ def bsm(spot, strike, expiry, volatility):
     time = abs((current - expiry).days)/365
     d1 = ((log(spot/strike)+((volatility**2)/2)*time))*((volatility*math.sqrt(time)))
     d3 = d1 - volatility*math.sqrt(time)
-    C =  scipy.stats.norm.cdf(d1)*spot - strike*exp(time)*scipy.stats.norm.cdf(d3)
-    return C
+    val =  scipy.stats.norm.cdf(d1)*spot - strike*exp(time)*scipy.stats.norm.cdf(d3)
+    return val
 
 date = datetime(2022, 8, 26, 12, 0, 0)
 print(bsm(20.5,20,date,0.05))
